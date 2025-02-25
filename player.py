94cb3fd1431eaf982e70eded91d6eea57bb01ac2
julsdgreat <jjbiado@gmail.com>
2025-02-25 14:59:17 +0800

    moving around

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 import circleshape
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -21,3 +21,6 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
